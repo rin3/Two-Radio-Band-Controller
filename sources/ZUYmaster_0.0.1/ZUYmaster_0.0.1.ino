3928b7b5542b28bbeadaac6488e53dc86179f887
Rin Fukuda <rin3jp@gmail.com>
2015-02-05 17:05:31 +0900

    .

--- sources/ZUYmaster_0.0.1/ZUYmaster_0.0.1.ino
+++ sources/ZUYmaster_0.0.1/ZUYmaster_0.0.1.ino
@@ -2,14 +2,48 @@
 	ZUYmaster
 	- a two radio band controller -
  
+ 	An Arduino firmware for a band controller in use with two radio HF contesting environment such as SO2R, M/S and M/2.
+ 
+ 	Limitatoins:
+  	This implimentation only works with newer Kenwood/ICOM exciters, two ICE-419B band pass filters and ICOM IC-PW1 amplifiers.
+ 	
+ 	The circuit:
  	Describe what it does in layman's terms.  Refer to the components
  	attached to the various pins.
- 
- 	The circuit:
+ 	
+  	The circuit consists of three boards:
+   	1 - Arduino Mega 2560 board
+    	1 - A Mega shield PCB with BPF I/F and IC-PW1 I/O
+     	1 - Another PCB for exciter (Kenwood/ICOM) I/O
 
-
- 	* list the components attached to each input
- 	* list the components attached to each output
+ 	* Input pins
+	  (none)
+ 	* Output pins
+ 	   4* - LED(green) - L Exciter status
+ 	   5* - LED(green) - R Exciter status
+ 	   2* - LED(red) - L IC-PW1 status
+ 	   3* - LED(red) - R IC-PW1 status
+	   7* - Tr array - L ICE-419 160m
+	   8* - Tr array - L ICE-419 80m
+	   9* - Tr array - L ICE-419 40m
+	  10* - Tr array - L ICE-419 20m
+	  11* - Tr array - L ICE-419 15m
+	  12* - Tr array - L ICE-419 10m
+	  A2* - Tr array - R ICE-419 160m
+	  A3* - Tr array - R ICE-419 80m
+	  A4* - Tr array - R ICE-419 40m
+	  A5* - Tr array - R ICE-419 20m
+	  A6* - Tr array - R ICE-419 15m
+	  A7* - Tr array - R ICE-419 10m
+ 	   6* - reed relay - L ICE-419 power control
+ 	  A0* - reed relay - R ICE-419 power control
+	  13* - DIP switch - L Exciter Kenwood/ICOM selection
+	  A1* - DIP switch - R Exciter Kenwood/ICOM selection
+ 	* Communications I/O
+ 	   0/Rx, 1/Tx - Serial  - L Exciter
+ 	  15/Rx,14/Tx - Serial3 - R Exciter
+ 	  19/Rx,18/Tx - Serial1 - L IC-PW1
+ 	  17/Rx,16/Tx - Serial2 - R IC-PW1
  
  	Created 4 Feb 2015
  	rin JG1VGX (jg1vgx@jarl.com)
